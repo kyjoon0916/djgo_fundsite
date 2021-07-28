0556eddde482f5cbde92d28ab6c9dc729d1dfca7
kyjoon0916 <kyjoon0916@naver.com>
2021-07-28 17:21:34 +0900

    scroll

--- p1/pybo/views.py
+++ p1/pybo/views.py
@@ -9,3 +9,15 @@ def index(request):
 
 def board(request):
     return render(request, 'board.html')
+
+def writing_list(request):
+    writing_list = writing.objects.all().order_by(‘-created_date’)
+    paginator = Paginator(post_list, 10)
+    page = request.POST.get(‘page’)
+
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)

6b5d04b2264458df95b63f10215d82af7ec70e4a
kyjoon0916 <kyjoon0916@naver.com>
2021-08-09 10:36:43 +0900

    0809_main

---
+++ main/p1/pybo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = [
+    path('', views.index),
+    path('about', views.about),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
